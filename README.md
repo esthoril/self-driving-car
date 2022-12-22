5d4f449f7f91b127651feef3fa061141907314ac
snhendri <sander.hendrickx@ugent.be>
2022-12-22 22:50:43 +0100

    lecture 1

---
+++ README.md
@@ -0,0 +1,3 @@
+## Self-driving car in Javascript
+
+Inspiration YouTube [Self-driving car - No libraries](https://www.youtube.com/watch?v=NkI9ia2cLhc&list=PLB0Tybl0UNfYoJE7ZwsBQoDIG4YN9ptyY)
---
+++ main.js
@@ -0,0 +1,16 @@
+const canvas = document.getElementById("myCanvas");
+canvas.width=1000;//200;
+
+const ctx = canvas.getContext("2d");
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+// animate();
+window.requestAnimationFrame(animate);
+
+function animate(){
+  canvas.height = window.innerHeight; // Hack to clear the canvas
+  car.update();
+  car.draw(ctx);
+  window.requestAnimationFrame(animate);
+}
